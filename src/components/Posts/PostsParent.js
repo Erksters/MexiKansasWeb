19aad42f7e4087b1af372cfbbf9cee6bc0732249
Erick <erksters@gmail.com>
2022-09-26 17:18:02 -0600

    added images to post

--- src/components/Posts/PostsParent.js
+++ src/components/Posts/PostsParent.js
@@ -7,15 +7,33 @@ const PostsParent = () => {
   const params = new URLSearchParams((window.location.hash.substring(7)));
   const post_id = params.get("id") || null;
 
+  function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+      images[item.replace("./", "")] = r(item);
+    });
+    return images;
+  }
+
+  const denseArrayImages = importAll(
+    require.context("./Images", false, /\.(png|jpe?g|svg)$/)
+  );
+  
   if (post_id !== null) {
     return (
       <div
         style={{
           display: "flex",
+          flexDirection:'column',
           width: "100vw",
           justifyContent: "center",
+          alignItems:'center'
         }}
       >
+        <br/>
+        <img style={{width:'400px', maxWidth: '75vw'}} src={`${denseArrayImages[ PostData[post_id].images[0]]}`} />
+        <br/>
+
         <Post post_id={post_id} />
       </div>
     );
