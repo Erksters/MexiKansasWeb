701cf8f64bb72efe2d95d4b8d378fa4b972f747e
Erick <erksters@gmail.com>
2022-09-26 14:40:09 -0600

    api config typo

--- src/components/api.js
+++ src/components/api.js
@@ -1,4 +1,4 @@
-// const base_url = "http://mexikansas.com/";
-const base_url = "http://localhost:3000/";
+const base_url = "https://www.mexikansas.com/";
+// const base_url = "http://localhost:3000/";
 
 export const search_post = base_url + "#/posts?id=";
